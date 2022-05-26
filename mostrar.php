eafee3618b7ff4c250e53c23e4074f6acfd5dff5
Dante Villanueva <104585946+DanteVL20@users.noreply.github.com>
2022-05-25 21:38:10 -0500

    Add files via upload

---
+++ mostrar.php
@@ -0,0 +1,33 @@
+<html>
+    <title>
+        
+    </title>
+    <body>
+        <?php
+        $respuesta1 = $_POST["respuesta1"];
+        echo "la respuesta 1 es: " .$respuesta1;
+        echo "<br>";
+        $respuesta2 = $_POST["respuesta2"];
+        echo "la respuesta 2 es: " .$respuesta2;
+        echo "<br>";
+        $respuesta3 = $_POST["respuesta3"];
+        echo "la respuesta 3 es: " .$respuesta3;
+        echo "<br>";
+        $respuesta4 = $_POST["respuesta4"];
+        echo "la respuesta 4 es: " .$respuesta4;
+        echo "<br>";
+        $respuesta5 = $_POST["respuesta5"];
+        echo "la respuesta 5 es: " .$respuesta5;
+        echo "<br>";
+        $respuesta6 = $_POST["respuesta6"];
+        echo "la respuesta 6 es: " .$respuesta6;
+        echo "<br>";
+        $respuesta7 = $_POST["respuesta7"];
+        echo "la respuesta 7 es: " .$respuesta7;
+        echo "<br>";
+        $respuesta8 = $_POST["respuesta8"];
+        echo "la respuesta 8 es: " .$respuesta8;
+        echo "<br>";
+        ?>
+    </body>
+</html>
